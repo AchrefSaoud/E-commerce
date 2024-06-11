227a573812887b27fb3de6c3120503c72e5b3e07
achraf <achraf.saoud@etudiant-fst.utm.tn>
2024-06-12 00:35:32 +0100

    first commit basic demo

---
+++ App/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeadpageComponent } from './headpage/headpage.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeadpageComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ App/src/app/headpage/headpage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headpage',
+  templateUrl: './headpage.component.html',
+  styleUrl: './headpage.component.css'
+})
+export class HeadpageComponent {
+
+}
